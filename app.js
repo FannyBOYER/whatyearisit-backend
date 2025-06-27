60c2f96bb73073b6f821d3363832f1bd08a3928f
Fanny <fannyboyer1d@gmail.com>
2025-06-27 11:13:12 +0200

    add year route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var yearsRouter = require('./routes/years');
+
 
 var app = express();
 
@@ -20,6 +20,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/years', yearsRouterRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/year', (req, res, next) => {
+  const date = new Date();
+  res.json({year : date.getFullYear()})
+});
+
 module.exports = router;
